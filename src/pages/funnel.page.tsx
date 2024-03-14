a28c0eda7f2e66834a0f56a30aba490af2b17f50
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-14 19:18:19 -0400

    Add new components and update content in FunnelPage

--- src/pages/funnel.page.tsx
+++ src/pages/funnel.page.tsx
@@ -1,10 +1,19 @@
 import FunnelLayout from '@src/components/custom/funnelLayout';
+import GridContent from '@src/components/custom/UI/grid/GridContent';
+import Heading from '@src/components/custom/UI/Heading';
 
 const FunnelPage = () => {
   return (
     <FunnelLayout>
       <div className="text-base-content">
-        <h1>Funnel</h1>
+        <Heading title="testing Headline" />
+        <GridContent
+          pageTitle="testing Page Title"
+          title1="testing Title 1"
+          title2="testing Title 2"
+          content1={<p>testing content 1</p>}
+          content2={<p>testing content 2</p>}
+        />
       </div>
     </FunnelLayout>
   );
