afe59e690fd53dd5bd88b4fbdc2aab23f131465c
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-15 19:08:16 -0400

    Refactor Grid2Contents component

--- src/components/custom/UI/grid/Grid2Contents.tsx
+++ src/components/custom/UI/grid/Grid2Contents.tsx
@@ -4,20 +4,22 @@ interface Props {}
 
 const GridContents: FC<Props> = (): JSX.Element => {
   return (
-    <form className="flex flex-col gap-2">
-      <label htmlFor="email" className="text-gray-700 text-sm font-bold">
-        Sign up for our newsletter:
-      </label>
-      <input
-        type="email"
-        id="email"
-        placeholder="Your email"
-        className="input-bordered input w-full max-w-xs"
-      />
-      <button type="submit" className="btn-primary btn">
-        Subscribe
-      </button>
-    </form>
+    <div className="rounded-md bg-base-100 p-3">
+      <form className="flex flex-col gap-2">
+        <label htmlFor="email" className="text-gray-700 text-sm font-bold">
+          Sign up for our newsletter:
+        </label>
+        <input
+          type="email"
+          id="email"
+          placeholder="Your email"
+          className="input-bordered input w-full max-w-xs"
+        />
+        <button type="submit" className="btn-primary btn">
+          Subscribe
+        </button>
+      </form>
+    </div>
   );
 };
 
