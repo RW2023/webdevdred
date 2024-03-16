b3f46a483d93577d2dfc79e1f1a80b1bb7f92bd6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-16 17:22:52 -0400

    Commented out Hero component in FunnelPage

--- src/pages/funnel/index.page.tsx
+++ src/pages/funnel/index.page.tsx
@@ -5,26 +5,26 @@ import Grid2Contents from '@src/components/custom/UI/grid/Grid2Contents';
 import Grid3Contents from '@src/components/custom/UI/grid/Grid3Contents';
 import Grid4Contents from '@src/components/custom/UI/grid/Grid4Contents';
 import GridContainer from '@src/components/custom/UI/grid/GridContainer';
-import Hero from '@src/components/custom/UI/Hero';
+// import Hero from '@src/components/custom/UI/Hero';
 
 const FunnelPage = () => {
   return (
     <FunnelLayout>
-      <Hero img="/assets/code.jpg">
-        <div className="text-base-content">
-          <GridContainer
-            pageTitle="Sign up for my newsletter!"
-            title1="Members Enjoy"
-            title2="Sign Up Now!"
-            title3="Join the Community"
-            title4="Get Started"
-            content1={<Grid1Contents />}
-            content2={<Grid2Contents />}
-            content3={<Grid3Contents />}
-            content4={<Grid4Contents />}
-          />
-        </div>
-      </Hero>
+      {/* <Hero img="/assets/code.jpg"> */}
+      <div className="text-base-content">
+        <GridContainer
+          pageTitle="Sign up for my newsletter!"
+          title1="Members Enjoy"
+          title2="Sign Up Now!"
+          title3="Join the Community"
+          title4="Get Started"
+          content1={<Grid1Contents />}
+          content2={<Grid2Contents />}
+          content3={<Grid3Contents />}
+          content4={<Grid4Contents />}
+        />
+      </div>
+      {/* </Hero> */}
     </FunnelLayout>
   );
 };
