abe852e67de64a30aef05fdda83c7be58b310c4b
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-16 14:32:26 -0400

    Update Grid4Contents styling to match other sibling components

--- src/components/custom/UI/grid/Grid4Contents.tsx
+++ src/components/custom/UI/grid/Grid4Contents.tsx
@@ -5,7 +5,7 @@ interface Props {}
 const Grid4Contents: FC<Props> = (): JSX.Element => {
   return (
     <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
-      <ul className="list-inside list-disc md:text-lg">
+      <ul className="list-inside list-disc rounded bg-base-300 p-3 md:text-lg">
         <li>Join our Discord community</li>
         <li>Collaborative projects to practice Open Source </li>
         <li>Code Review and Mentorship </li>
