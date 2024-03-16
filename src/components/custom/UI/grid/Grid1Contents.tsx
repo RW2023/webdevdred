e7f8463e140cfa8c824bc5724fc93c8897baad22
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-16 10:38:53 -0400

    add more content to test

--- src/components/custom/UI/grid/Grid1Contents.tsx
+++ src/components/custom/UI/grid/Grid1Contents.tsx
@@ -1,17 +1,31 @@
 import { FC } from 'react';
 
+import Hero from '@src/components/custom/UI/Hero';
+import SubHeading from '@src/components/custom/UI/SubHeading';
+
 interface Props {}
 
 const GridContents: FC<Props> = (): JSX.Element => {
   return (
-    <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
-      <ul className="list-inside list-disc md:text-lg">
-        <li>Exclusive discounts</li>
-        <li>Access to member only Resources</li>
-        <li>Weekly Newsletter with tips and tricks</li>
-        <li>Active Community</li>
-      </ul>
-    </div>
+    <Hero img="/assets/club.jpg">
+      <div>
+        <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
+          <ul className="list-inside list-disc md:text-lg">
+            <li>Exclusive discounts</li>
+            <li>Access to member only Resources</li>
+            <li>Weekly Newsletter with tips and tricks</li>
+            <li>Active Community</li>
+          </ul>
+        </div>
+        <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
+          <SubHeading title="Member Perks" />
+          <ul className="list-inside list-disc md:text-lg">
+            <li>Access New content early</li>
+            <li>Discounts and courses and resources</li>
+          </ul>
+        </div>
+      </div>
+    </Hero>
   );
 };
 
