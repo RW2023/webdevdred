95faecf8d3160811390768b766376465453fe6f4
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-16 14:30:47 -0400

    Update list styling in Grid1Contents and Grid3Contents

--- src/components/custom/UI/grid/Grid1Contents.tsx
+++ src/components/custom/UI/grid/Grid1Contents.tsx
@@ -10,7 +10,7 @@ const GridContents: FC<Props> = (): JSX.Element => {
     <Hero img="/assets/club.jpg">
       <div>
         <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
-          <ul className="list-inside list-disc md:text-lg">
+          <ul className="list-inside list-disc rounded bg-base-300 p-3 md:text-lg">
             <li>Exclusive discounts</li>
             <li>Access to member only Resources</li>
             <li>Weekly Newsletter with tips and tricks</li>
@@ -19,7 +19,7 @@ const GridContents: FC<Props> = (): JSX.Element => {
         </div>
         <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
           <SubHeading title="Member Perks" />
-          <ul className="list-inside list-disc md:text-lg">
+          <ul className="list-inside list-disc rounded bg-base-300 p-3 md:text-lg">
             <li>Access New content early</li>
             <li>Discounts and courses and resources</li>
           </ul>
--- src/components/custom/UI/grid/Grid3Contents.tsx
+++ src/components/custom/UI/grid/Grid3Contents.tsx
@@ -8,7 +8,7 @@ const Grid3Contents: FC<Props> = (): JSX.Element => {
   return (
     <Hero img="/assets/community.jpg">
       <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
-        <ul className="list-inside list-disc md:text-lg">
+        <ul className="list-inside list-disc rounded bg-base-300 p-3 md:text-lg">
           <li>Join our Discord community</li>
           <li>Collaborative projects to practice Open Source </li>
           <li>Code Review and Mentorship </li>
