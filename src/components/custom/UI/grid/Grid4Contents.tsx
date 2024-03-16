758aa772106db07300aac983305fd5d8d7533b1f
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-03-16 10:02:03 -0400

    Add Grid3Contents and Grid4Contents components

---
+++ src/components/custom/UI/grid/Grid4Contents.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+interface Props {}
+
+const Grid4Contents: FC<Props> = (): JSX.Element => {
+  return (
+    <div className="m-1 rounded-md bg-base-100 p-4 text-base-content shadow-lg">
+      <ul className="list-inside list-disc md:text-lg">
+        <li>Join our Discord community</li>
+        <li>Collaborative projects to practice Open Source </li>
+        <li>Code Review and Mentorship </li>
+        <li>Networking opportunities</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Grid4Contents;
