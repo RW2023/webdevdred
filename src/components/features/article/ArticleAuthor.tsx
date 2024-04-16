3dcf665c3ce2027a3d6d5ba13833509d418f6e97
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-16 18:37:47 -0400

    set text to base content

--- src/components/features/article/ArticleAuthor.tsx
+++ src/components/features/article/ArticleAuthor.tsx
@@ -15,7 +15,8 @@ export const ArticleAuthor = ({ article }: ArticleAuthorProps) => {
     <div className="flex items-center">
       <div
         className="mr-2 overflow-hidden rounded-full border border-blue500"
-        {...inspectorProps({ fieldId: 'avatar' })}>
+        {...inspectorProps({ fieldId: 'avatar' })}
+      >
         {author?.avatar && (
           <CtfImage
             nextImageProps={{
@@ -28,7 +29,10 @@ export const ArticleAuthor = ({ article }: ArticleAuthorProps) => {
           />
         )}
       </div>
-      <span className="text-xs leading-none text-gray600" {...inspectorProps({ fieldId: 'name' })}>
+      <span
+        className="text-xs leading-none text-base-content"
+        {...inspectorProps({ fieldId: 'name' })}
+      >
         {author?.name}
       </span>
     </div>
