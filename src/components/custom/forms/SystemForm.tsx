be0173f1dc91b00cd4a5f3a4d38a20c8f201b5da
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-16 15:15:05 -0400

    Refactor SystemForm component in SystemForm.tsx

--- src/components/custom/forms/SystemForm.tsx
+++ src/components/custom/forms/SystemForm.tsx
@@ -1,61 +1,56 @@
-'use client';
-import { FC } from 'react';
+import React from 'react';
+import SubHeading from '@src/components/custom/UI/SubHeading';
 
-interface Props {}
-
-const SystemForm: FC<Props> = (): JSX.Element => {
+function SubscriptionForm() {
   return (
-    <form
-      method="post"
-      action="https://systeme.io/embedded/15714774/subscription"
-      className="rounded-lg bg-base-100 p-4 shadow-lg"
-    >
-      <div className="form-control">
-        <label className="label" htmlFor="first_name">
-          <span className="label-text">First Name</span>
-        </label>
-        <input
-          type="text"
-          name="first_name"
-          id="first_name"
-          placeholder="First Name"
-          className="input-bordered input"
-        />
-      </div>
-
-      <div className="form-control">
-        <label className="label" htmlFor="surname">
-          <span className="label-text">Last Name</span>
-        </label>
-        <input
-          type="text"
-          name="surname"
-          id="surname"
-          placeholder="Last Name"
-          className="input-bordered input"
-        />
-      </div>
-
-      <div className="form-control">
-        <label className="label" htmlFor="email">
-          <span className="label-text">Email</span>
-        </label>
-        <input
-          type="email"
-          name="email"
-          id="email"
-          placeholder="Email"
-          className="input-bordered input"
-        />
-      </div>
-
-      <div className="form-control mt-4">
-        <button type="submit" className="btn-primary btn">
-          Subscribe
-        </button>
-      </div>
-    </form>
+    <div>
+      <SubHeading title="Subscribe to our newsletter" />
+      <form
+        method="post"
+        action="https://systeme.io/embedded/15714774/subscription"
+        className="bg-white mb-4 rounded px-8 pt-6 pb-8 shadow-md"
+      >
+        <div className="mb-4">
+          <label className="text-gray-700 mb-2 block text-sm font-bold" htmlFor="surname">
+            Surname
+          </label>
+          <input
+            type="text"
+            name="surname"
+            id="surname"
+            className="text-gray-700 focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-700 mb-2 block text-sm font-bold" htmlFor="email">
+            Email
+          </label>
+          <input
+            type="text"
+            name="email"
+            id="email"
+            className="text-gray-700 focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="text-gray-700 mb-2 block text-sm font-bold" htmlFor="first_name">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="first_name"
+            id="first_name"
+            className="text-gray-700 focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button type="submit" className="btn-primary btn">
+            Subscribe
+          </button>
+        </div>
+      </form>
+    </div>
   );
-};
+}
 
-export default SystemForm;
+export default SubscriptionForm;
