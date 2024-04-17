8743deeeb4620441a91997d8c01953cf3cee7539
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-16 22:08:19 -0400

    Update text styling in SystemForm.tsx

--- src/components/custom/forms/SystemForm.tsx
+++ src/components/custom/forms/SystemForm.tsx
@@ -1,50 +1,52 @@
 import React from 'react';
+
 import SubHeading from '@src/components/custom/UI/SubHeading';
 
 function SubscriptionForm() {
   return (
     <div>
       <SubHeading title="Subscribe to our newsletter" />
+      <p className="text-center">Keep up to date with new posts </p>
       <form
         method="post"
         action="https://systeme.io/embedded/15714774/subscription"
-        className="bg-white mb-4 rounded px-8 pt-6 pb-8 shadow-md"
+        className="mb-4 rounded-md bg-base-100 px-8 pt-6 pb-8 shadow-md"
       >
         <div className="mb-4">
-          <label className="text-base-content mb-2 block text-sm font-bold" htmlFor="first_name">
+          <label className="mb-2 block text-sm font-bold text-base-content" htmlFor="first_name">
             First Name
           </label>
           <input
             type="text"
             name="first_name"
             id="first_name"
-            className="text-base-content focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
+            className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-base-content shadow focus:outline-none"
           />
         </div>
         <div className="mb-4">
-          <label className="text-base-content mb-2 block text-sm font-bold" htmlFor="surname">
+          <label className="mb-2 block text-sm font-bold text-base-content" htmlFor="surname">
             Last Name
           </label>
           <input
             type="text"
             name="surname"
             id="surname"
-            className="text-base-content focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
+            className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-base-content shadow focus:outline-none"
           />
         </div>
         <div className="mb-4">
-          <label className="text-base-content mb-2 block text-sm font-bold" htmlFor="email">
+          <label className="mb-2 block text-sm font-bold text-base-content" htmlFor="email">
             Email
           </label>
           <input
-            type="email"  // Changed type to email for proper validation
+            type="email" // Changed type to email for proper validation
             name="email"
             id="email"
-            className="text-base-content focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight shadow focus:outline-none"
+            className="focus:shadow-outline w-full appearance-none rounded border py-2 px-3 leading-tight text-base-content shadow focus:outline-none"
           />
         </div>
         <div className="flex items-center justify-between">
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn-primary btn">
             Subscribe
           </button>
         </div>
