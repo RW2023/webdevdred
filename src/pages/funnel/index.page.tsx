0c5737330c0523ee382b299c50dc940bd1da7b84
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-17 17:42:28 -0400

    Refactor FunnelPage component to use SystemForm and add Home button

--- src/pages/funnel/index.page.tsx
+++ src/pages/funnel/index.page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import SystemForm from '../../components/custom/forms/SystemForm';
 import Hero from '../../components/custom/UI/Hero';
 
@@ -5,8 +7,13 @@ const FunnelPage = () => {
   return (
     // <Hero img="/assets/code.jpg">
     <Hero img="https://images.ctfassets.net/dnd5s50dhx3v/76reejLvctB6ZYSnCtkAfr/0bc90a6331ca233938271135160d361e/JungleDred.webp">
-      <div className="container flex flex-1 items-center justify-center text-base-content">
+      <div className="container flex flex-1 flex-col items-center justify-center text-base-content">
         <SystemForm />
+        <Link href={'/'}>
+          <button type="button" className="btn-primary btn">
+            Home
+          </button>
+        </Link>
       </div>
     </Hero>
   );
