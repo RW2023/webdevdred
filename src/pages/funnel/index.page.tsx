7987e9a7812a7e6dc14621fc0aeb04e17b0d7edf
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-17 17:12:39 -0400

    Update FunnelPage to use direct from contentfull image URL

--- src/pages/funnel/index.page.tsx
+++ src/pages/funnel/index.page.tsx
@@ -4,7 +4,7 @@ import Hero from '../../components/custom/UI/Hero';
 const FunnelPage = () => {
   return (
     // <Hero img="/assets/code.jpg">
-    <Hero img="https://webdevdred.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fdnd5s50dhx3v%2F76reejLvctB6ZYSnCtkAfr%2F0bc90a6331ca233938271135160d361e%2FJungleDred.webp&w=2048&q=75">
+    <Hero img="https://images.ctfassets.net/dnd5s50dhx3v/76reejLvctB6ZYSnCtkAfr/0bc90a6331ca233938271135160d361e/JungleDred.webp">
       <div className="container flex flex-1 items-center justify-center text-base-content">
         <SystemForm />
       </div>
