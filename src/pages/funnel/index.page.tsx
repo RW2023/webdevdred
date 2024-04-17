88781399900262c9e6eba4b57ffa6233b3c72a07
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-17 09:36:20 -0400

    Update FunnelPage to use SystemForm instead of GridContainer

--- src/pages/funnel/index.page.tsx
+++ src/pages/funnel/index.page.tsx
@@ -1,31 +1,12 @@
-import FunnelLayout from './funnelLayout';
+import SystemForm from '../../components/custom/forms/SystemForm';
 
-import Grid1Contents from '@src/components/custom/UI/grid/Grid1Contents';
-import Grid2Contents from '@src/components/custom/UI/grid/Grid2Contents';
-import Grid3Contents from '@src/components/custom/UI/grid/Grid3Contents';
-import Grid4Contents from '@src/components/custom/UI/grid/Grid4Contents';
-import GridContainer from '@src/components/custom/UI/grid/GridContainer';
-// import Hero from '@src/components/custom/UI/Hero';
 
 const FunnelPage = () => {
   return (
-    <FunnelLayout>
-      {/* <Hero img="/assets/code.jpg"> */}
-      <div className="text-base-content">
-        <GridContainer
-          pageTitle="Sign up for my newsletter!"
-          title1="Members Enjoy"
-          title2="Sign Up Now!"
-          title3="Join the Community"
-          title4="Get Started"
-          content1={<Grid1Contents />}
-          content2={<Grid2Contents />}
-          content3={<Grid3Contents />}
-          content4={<Grid4Contents />}
-        />
-      </div>
-      {/* </Hero> */}
-    </FunnelLayout>
+    // <Hero img="/assets/code.jpg">
+    <div className="text-base-content container flex flex-1 justify-center items-center">
+      <SystemForm />
+    </div>
   );
 };
 
