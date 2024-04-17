d0a939c3ed34d6bd1a941d401372260ca3b8b0e6
RE. Wilson <121303360+RW2023@users.noreply.github.com>
2024-04-17 17:07:35 -0400

    Update FunnelPage to use Hero component and add image URL

--- src/pages/funnel/index.page.tsx
+++ src/pages/funnel/index.page.tsx
@@ -1,11 +1,14 @@
 import SystemForm from '../../components/custom/forms/SystemForm';
+import Hero from '../../components/custom/UI/Hero';
 
 const FunnelPage = () => {
   return (
     // <Hero img="/assets/code.jpg">
-    <div className="container flex flex-1 items-center justify-center text-base-content">
-      <SystemForm />
-    </div>
+    <Hero img="https://webdevdred.vercel.app/_next/image?url=https%3A%2F%2Fimages.ctfassets.net%2Fdnd5s50dhx3v%2F76reejLvctB6ZYSnCtkAfr%2F0bc90a6331ca233938271135160d361e%2FJungleDred.webp&w=2048&q=75">
+      <div className="container flex flex-1 items-center justify-center text-base-content">
+        <SystemForm />
+      </div>
+    </Hero>
   );
 };
 
